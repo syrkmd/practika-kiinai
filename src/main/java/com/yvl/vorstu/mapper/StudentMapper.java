533477b46e6dfbfaabb91bc48268de66529dffba
Yurii <yvla@MacBook-Pro-Yurii.local>
2026-06-28 16:03:35 +0400

    replace manual mapper with MapStruct and migrate to  postgres

--- src/main/java/com/yvl/vorstu/mapper/StudentMapper.java
+++ src/main/java/com/yvl/vorstu/mapper/StudentMapper.java
@@ -4,39 +4,21 @@ import com.yvl.vorstu.dto.student.request.CreateStudentRequest;
 import com.yvl.vorstu.dto.student.request.UpdateStudentRequest;
 import com.yvl.vorstu.dto.student.response.StudentResponse;
 import com.yvl.vorstu.entities.Student;
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 import java.util.List;
 
-@Component
-public class StudentMapper {
+@Mapper(componentModel = "spring")
+public interface StudentMapper {
 
-    public Student toEntity(CreateStudentRequest request) {
-        return Student.builder()
-                .fio(request.getFio())
-                .group(request.getGroup())
-                .phoneNumber(request.getPhoneNumber())
-                .build();
-    }
+    Student toEntity(CreateStudentRequest request);
 
-    public StudentResponse toResponse(Student student) {
-        return StudentResponse.builder()
-                .id(student.getId())
-                .fio(student.getFio())
-                .group(student.getGroup())
-                .phoneNumber(student.getPhoneNumber())
-                .build();
-    }
+    StudentResponse toResponse(Student student);
 
-    public void updateEntity(Student student, UpdateStudentRequest request) {
-        student.setFio(request.getFio());
-        student.setGroup(request.getGroup());
-        student.setPhoneNumber(request.getPhoneNumber());
-    }
+    List<StudentResponse> toResponseList(List<Student> students);
 
-    public List<StudentResponse> toResponseList(List<Student> students) {
-        return students.stream()
-                .map(this::toResponse)
-                .toList();
-    }
+    @Mapping(target = "id", ignore = true)
+    void updateEntity(@MappingTarget Student student, UpdateStudentRequest request);
 }
